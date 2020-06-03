3f9003c73d0b70be5083584594d885d495ec6876
New Media Works <oss@newmediaworks.com>
2020-06-03 17:18:22 -0500

    Now using String varargs instead of generic Map

--- book/src/main/java/com/newmediaworks/taglib/payment/book/NmwPaymentTldInitializer.java
+++ book/src/main/java/com/newmediaworks/taglib/payment/book/NmwPaymentTldInitializer.java
@@ -23,7 +23,6 @@
 package com.newmediaworks.taglib.payment.book;
 
 import com.semanticcms.tagreference.TagReferenceInitializer;
-import java.util.Collections;
 
 public class NmwPaymentTldInitializer extends TagReferenceInitializer {
 
@@ -37,7 +36,7 @@ public class NmwPaymentTldInitializer extends TagReferenceInitializer {
 			Maven.properties.getProperty("documented.javadoc.link.javase"),
 			Maven.properties.getProperty("documented.javadoc.link.javaee"),
 			// Self
-			Collections.singletonMap("com.newmediaworks.taglib.payment", Maven.properties.getProperty("project.url") + "apidocs/")
+			"com.newmediaworks.taglib.payment", Maven.properties.getProperty("project.url") + "apidocs/"
 		);
 	}
 }
