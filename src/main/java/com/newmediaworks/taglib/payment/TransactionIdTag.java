334282746719b59a667586567093090ea4b909e7
New Media Works <oss@newmediaworks.com>
2020-08-19 22:31:42 -0500

    Fixed typo in project name in top comments

--- src/main/java/com/newmediaworks/taglib/payment/TransactionIdTag.java
+++ src/main/java/com/newmediaworks/taglib/payment/TransactionIdTag.java
@@ -1,24 +1,24 @@
 /*
- * new-payment-taglib - JSP taglib encapsulating the AO Credit Cards API.
+ * nmw-payment-taglib - JSP taglib encapsulating the AO Credit Cards API.
  * Copyright (C) 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2019  New Media Works
  *     info@newmediaworks.com
  *     703 2nd Street #465
  *     Santa Rosa, CA 95404
  *
- * This file is part of new-payment-taglib.
+ * This file is part of nmw-payment-taglib.
  *
- * new-payment-taglib is free software: you can redistribute it and/or modify
+ * nmw-payment-taglib is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * new-payment-taglib is distributed in the hope that it will be useful,
+ * nmw-payment-taglib is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with new-payment-taglib.  If not, see <http://www.gnu.org/licenses/>.
+ * along with nmw-payment-taglib.  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.newmediaworks.taglib.payment;
 
