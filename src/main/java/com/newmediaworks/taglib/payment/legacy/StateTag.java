80fede5704d6e51ff6d39304e8ca0930cfa0a3fd
New Media Works <oss@newmediaworks.com>
2021-10-30 23:43:24 -0500

    Changed links to https for https://www.gnu.org/licenses/

--- src/main/java/com/newmediaworks/taglib/payment/legacy/StateTag.java
+++ src/main/java/com/newmediaworks/taglib/payment/legacy/StateTag.java
@@ -18,7 +18,7 @@
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with nmw-payment-taglib.  If not, see <http://www.gnu.org/licenses/>.
+ * along with nmw-payment-taglib.  If not, see <https://www.gnu.org/licenses/>.
  */
 package com.newmediaworks.taglib.payment.legacy;
 
