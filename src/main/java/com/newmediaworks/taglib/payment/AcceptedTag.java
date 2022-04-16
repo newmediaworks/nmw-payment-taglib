5c687b401367348bfe09931a76759d78a9d59c73
New Media Works <oss@newmediaworks.com>
2022-04-16 02:15:06 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- src/main/java/com/newmediaworks/taglib/payment/AcceptedTag.java
+++ src/main/java/com/newmediaworks/taglib/payment/AcceptedTag.java
@@ -1,6 +1,6 @@
 /*
  * nmw-payment-taglib - JSP taglib encapsulating the AO Payments API.
- * Copyright (C) 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2019, 2020, 2021  New Media Works
+ * Copyright (C) 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2019, 2020, 2021, 2022  New Media Works
  *     info@newmediaworks.com
  *     703 2nd Street #465
  *     Santa Rosa, CA 95404
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with nmw-payment-taglib.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.newmediaworks.taglib.payment;
 
 import com.aoapps.payments.AuthorizationResult;
